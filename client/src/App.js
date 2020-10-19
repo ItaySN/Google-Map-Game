e1440622cdec268be0ff31c2213507234f75d09f
ItaySN <itayfic@gmail.com>
2020-10-19 22:24:39 +0300

    Create React App

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Map from './components/Map';
+import './components/map.css'
+import StartMenu from './components/StartMenu';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
